19f2dfdd557ce9d58410d44ea8545d0825eb4e82
Sicong Qian <sicong.qian@dena.com>
2015-06-16 18:47:21 +0800

    no message

--- GDFamily/ViewController.m
+++ GDFamily/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import <MAMapKit/MAMapKit.h>
+#import <AMapSearchKit/AMapSearchAPI.h>
 
 @interface ViewController ()<MAMapViewDelegate>
 
@@ -21,7 +22,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds))];
+    _mapView.pausesLocationUpdatesAutomatically = NO;
+    _mapView.showsUserLocation = YES;
     _mapView.delegate = self;
+    
+    _mapView.userTrackingMode = MAUserTrackingModeFollow;
     [self.view addSubview:_mapView];
 }
 
@@ -29,5 +34,35 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+static long record = 0;
+-(void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation
+updatingLocation:(BOOL)updatingLocation
+{
+    if(updatingLocation)
+    {
+        //取出当前位置的坐标
+        NSLog(@"latitude : %f,longitude: %f",userLocation.coordinate.latitude,userLocation.coordinate.longitude);
+        record++;
+        NSLog(@"record: %ld",record);
+    }
+}
 
+- (MAAnnotationView *)mapView:(MAMapView *)mapView viewForAnnotation:(id <MAAnnotation>)annotation
+{
+    /* 自定义userLocation对应的annotationView. */
+    if ([annotation isKindOfClass:[MAUserLocation class]])
+    {
+        static NSString *userLocationStyleReuseIndetifier = @"userLocationStyleReuseIndetifier";
+        MAAnnotationView *annotationView = [mapView dequeueReusableAnnotationViewWithIdentifier:userLocationStyleReuseIndetifier];
+        if (annotationView == nil)
+        {
+            annotationView = [[MAAnnotationView alloc] initWithAnnotation:annotation
+                                                          reuseIdentifier:userLocationStyleReuseIndetifier];
+        }
+        annotationView.image = [UIImage imageNamed:@"userPosition"];
+        
+        return annotationView;
+    }
+    return nil;
+}
 @end
